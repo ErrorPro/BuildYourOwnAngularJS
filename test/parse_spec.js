ff9f4550c92d837075a4ef0a94515360690226f2
Ven Korolev <mohtaiiihuk@gmail.com>
2016-02-05 15:53:34 +0700

    parse

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -39,6 +39,104 @@ describe('parse', function() {
   });
 
   it('can parse scientific notation with the + sign', function() {
+    var fn = parse('.42e+2');
+    expect(fn()).toBe(42);
+  });
 
+  it('can parse upcase scientific notation', function() {
+    var fn = parse('.42E2');
+    expect(fn()).toBe(42);
+  });
+
+  it('will not parse invalid scientific notation', function() {
+    expect(function() { parse('42e-'); }).toThrow();
+    expect(function() { parse('42e-a'); }).toThrow();
+  });
+
+  it('can parse a string in single quotes', function() {
+    var fn = parse("'abc'");
+    expect(fn()).toBe('abc');
+  });
+
+  it('can parse a string in double quotes', function() {
+    var fn = parse('"abc"');
+    expect(fn()).toBe('abc');
+  });
+
+  it('will not parse a string with mismatching quotes', function() {
+    expect(function() { parse('"abc\'')}).toThrow();
+  });
+
+  it('marks strings as literal and constant', function() {
+    var fn = parse('"abc"');
+    expect(fn.literal).toBe(true);
+    expect(fn.constant).toBe(true);
+  });
+
+  it('will parse a string with character escapes', function() {
+    var fn = parse('"\\n\\r\\\\"');
+    expect(fn()).toEqual('\n\r\\')
+  });
+
+  it('will parse a string with unicode escapes', function() {
+    var fn = parse('"\\u00A0"');
+    expect(fn()).toEqual('\u00A0');
+  });
+
+  it('will not parse a string with invalid unicode escapes', function() {
+    expect(function() { parse ('"\\u00T0"') }).toThrow();
+  });
+
+  it('will parse null', function() {
+    var fn = parse('null');
+    expect(fn()).toBe(null);
+  });
+
+  it('will parse true', function() {
+    var fn = parse('true');
+    expect(fn()).toBe(true);
+  });
+
+  it('will parse false', function() {
+    var fn = parse('false');
+    expect(fn()).toBe(false);
+  });
+
+  it('marks booleans as literal and constant', function() {
+    var fn = parse('true');
+    expect(fn.literal).toBe(true);
+    expect(fn.constant).toBe(true);
+  });
+
+  it('marks null as literal and constant', function() {
+    var fn = parse('null');
+    expect(fn.literal).toBe(true);
+    expect(fn.constant).toBe(true);
+  });
+
+  it('ignores whitespaces', function() {
+    var fn = parse(' \n42 ');
+    expect(fn()).toEqual(42);
+  });
+
+  it('will parse an empty array', function() {
+    var fn = parse('[]');
+    expect(fn()).toEqual([]);
+  });
+
+  it('will parse a non-empty array', function() {
+    var fn = parse('[1, "two", [3]]');
+    expect(fn()).toEqual([1, 'two', [3]]);
+  });
+
+  it('will parse an array with trailing commas', function() {
+    var fn = parse('[1, 2, 3, ]');
+    expect(fn()).toEqual([1, 2, 3]);
+  });
+
+  it('marks array literals as literal and constant', function() {
+    var fn = parse('[1, 2, 3]');
+    expect(fn.literal).toBe(true);
+    expect(fn.constant).toBe(true);
   })
 })
